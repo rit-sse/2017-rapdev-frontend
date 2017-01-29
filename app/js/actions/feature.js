31c3f9ae30a268fcfdfcd35a34576f9e14a8b97c
Michael Timbrook <timbrook480@gmail.com>
2017-01-29 16:22:33 -0500

    Added feature

--- app/js/actions/feature.js
+++ app/js/actions/feature.js
@@ -1,7 +1,15 @@
+import { api } from './utils';
+
 export const SET_ALL_FEATURE = 'SET_ALL_FEATURE';
 
 export function loadRoomFeatures() {
     return (dispatch) => {
-
+        return dispatch(api('/api/v1/feature'))
+            .then(feature => {
+                dispatch({
+                    type: SET_ALL_FEATURE,
+                    feature
+                });
+            });
     };
 }
--- app/js/reducers/feature.js
+++ app/js/reducers/feature.js
@@ -2,24 +2,7 @@ import {
     SET_ALL_FEATURE
 } from '../actions/feature';
 
-export default function feature(state = [
-    {
-        id: 0,
-        name: 'Have TV'
-    },
-    {
-        id: 1,
-        name: 'Has Projector'
-    },
-    {
-        id: 3,
-        name: 'Webcam'
-    },
-    {
-        id: 4,
-        name: 'Sterile'
-    },
-], action) {
+export default function feature(state = [], action) {
     switch (action.type) {
         case SET_ALL_FEATURE:
             return action.feature;
